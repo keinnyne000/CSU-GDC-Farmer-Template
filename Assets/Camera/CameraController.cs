466e6c6f6dfec40f36531bc5356050acaf33883c
Ryan Stevens <keinnyne@gmail.com>
2025-02-11 10:39:15 -0700

    remove unused field in camera controller

--- Assets/Camera/CameraController.cs
+++ Assets/Camera/CameraController.cs
@@ -5,7 +5,6 @@ namespace Farmer.Camera
     public class CameraController : MonoBehaviour
     {
         [SerializeField] GameObject player;
-        [SerializeField] private float lerpStrength = 0.1f;
         
         void LateUpdate()
         {
