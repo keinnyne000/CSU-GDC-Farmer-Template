77f14eeb0d68b0388e6f731be2d842469d7cd400
Ryan Stevens <keinnyne@gmail.com>
2025-02-14 13:14:08 -0700

    add comments to camera classes

--- Assets/Camera/CameraController.cs
+++ Assets/Camera/CameraController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 namespace Farmer.Camera
 {
+    // This class is responsible for controlling the camera
+    // It should be attached (or a parent of) the camera object
     public class CameraController : MonoBehaviour
     {
         [SerializeField] GameObject player;
